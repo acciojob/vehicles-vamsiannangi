1806747f1de0206242bf40289e73e8353c53ffb3
=vamsi <=>
2023-09-16 12:31:55 +0530

    initial commite

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class Boat implements WaterVehicle {
+    String name;
+    int capacity;
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
